e814a92625717d0f4fd1ef3758861583daae2656
Guilherme Virtuoso <guilhermevirtuoso83@gmail.com>
2015-08-17 15:19:51 -0300

    Ajustando point-end da aplicacao

--- src/main/webapp/js/values/configValue.js
+++ src/main/webapp/js/values/configValue.js
@@ -1,4 +1,4 @@
 angular.module("listaCompras").value("config", {
-	baseUrl: "http://localhost:8080/app" // LOCAL
-	//baseUrl: "http://52.20.90.29:28080" // PRODUCAO
+	//baseUrl: "http://localhost:8080/app" // LOCAL
+	baseUrl: "http://52.20.90.29:28080" // PRODUCAO
 });
